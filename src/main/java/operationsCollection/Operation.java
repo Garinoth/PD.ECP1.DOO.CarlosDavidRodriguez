16c56d1d553ea9a5d185eb96f2dccdf7ac76e67e
CDavidRP <cdavidrp@gmail.com>
2014-10-06 13:53:02 +0200

    First commit

---
+++ src/main/java/operationsCollection/Operation.java
@@ -0,0 +1,5 @@
+package operationsCollection;
+
+public class Operation {
+
+}
---
+++ src/main/java/operationsCollection/Operations.java
@@ -0,0 +1,22 @@
+package operationsCollection;
+
+public class Operations {
+
+    private Operation[] operations;
+
+    public Operations() {
+        this.operations = new Operation[10];
+    }
+    
+    public void add() {
+        
+    }
+    
+    public void reset() {
+        
+    }
+    
+    public int total() {
+        return 0;
+    }
+}
