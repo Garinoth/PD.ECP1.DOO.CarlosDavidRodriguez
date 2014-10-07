3ced6347fe4ebbb8dcded7093df6c4bd5a9f2201
CDavidRP <cdavidrp@gmail.com>
2014-10-07 11:18:43 +0200

    Implementar clase Operations

--- src/main/java/operationsCollection/Operations.java
+++ src/main/java/operationsCollection/Operations.java
@@ -2,21 +2,32 @@ package operationsCollection;
 
 public class Operations {
 
+    private final int SIZE = 10;
     private Operation[] operations;
 
     public Operations() {
-        this.operations = new Operation[10];
+        this.operations = new Operation[this.SIZE];
     }
     
-    public void add() {
-        
+    public void add(Operation operation) {
+        int i = 0;
+        while (i < this.operations.length && this.operations[i] != null) {
+            i++;
+        }
+        if (i < this.operations.length) {
+            this.operations[i] = operation;
+        }
     }
     
     public void reset() {
-        
+        this.operations = new Operation[this.SIZE];
     }
     
     public int total() {
-        return 0;
+        int res = 0;
+        for (Operation operation : operations) {
+            res += operation.operate();
+        }
+        return res;
     }
 }
