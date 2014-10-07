b5a3cdc2b66eea0f56e52b7bce5b5f790db6bdec
CDavidRP <cdavidrp@gmail.com>
2014-10-07 12:39:55 +0200

    Add inheritance to tests

--- src/test/java/operationsCollection/SummationTest.java
+++ src/test/java/operationsCollection/SummationTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SummationTest {
+public class SummationTest extends OperationTest {
     
     private Summation summation;
     
