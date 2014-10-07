bd5cc90c624f6d013c3e984df12d4238083ab68b
CDavidRP <cdavidrp@gmail.com>
2014-10-07 12:26:13 +0200

    Implementar test de multiplicación

---
+++ src/test/java/operationsCollection/MultiplicationTest.java
@@ -0,0 +1,22 @@
+package operationsCollection;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MultiplicationTest {
+    
+    private Multiplication multiplication;
+    
+    @Before
+    public void before() {
+        this.multiplication = new Multiplication(3, 5);
+    }
+
+    @Test
+    public void testOperate() {
+        assertEquals(15, this.multiplication.operate(), 10e-1);
+    }
+
+}
