82158208bff5ab13e8513a1288159ab49cad2f2c
CDavidRP <cdavidrp@gmail.com>
2014-10-07 10:43:40 +0200

    Implementar clase Operation

--- src/main/java/operationsCollection/Operation.java
+++ src/main/java/operationsCollection/Operation.java
@@ -1,5 +1,23 @@
 package operationsCollection;
 
-public class Operation {
+public abstract class Operation {
+    
+    private int operator1;
+    private int operator2;
+    
+    public Operation(int operator1, int operator2) {
+        super();
+        this.operator1 = operator1;
+        this.operator2 = operator2;
+    }
+    
+    public int getOperator1() {
+        return operator1;
+    }
 
+    public int getOperator2() {
+        return operator2;
+    }
+
+    public abstract int operate();
 }
