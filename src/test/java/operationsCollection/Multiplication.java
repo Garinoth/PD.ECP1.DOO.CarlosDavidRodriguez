6294b52de48999a3caa572444c5298d759638a17
CDavidRP <cdavidrp@gmail.com>
2014-10-07 11:18:31 +0200

    Incluir clases hijas concretas de Operation

---
+++ src/test/java/operationsCollection/Multiplication.java
@@ -0,0 +1,15 @@
+package operationsCollection;
+
+public class Multiplication extends Operation {
+
+    public Multiplication(int operator1, int operator2) {
+        super(operator1, operator2);
+    }
+
+    @Override
+    public int operate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ src/test/java/operationsCollection/Summation.java
@@ -0,0 +1,15 @@
+package operationsCollection;
+
+public class Summation extends Operation {
+
+    public Summation(int operator1, int operator2) {
+        super(operator1, operator2);
+    }
+
+    @Override
+    public int operate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
