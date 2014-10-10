3d9b44c84ebd32cc52a464e8993a9c4bb0e673f8
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2014-10-10 19:05:00 +0200

    Implementar Gestor

--- src/main/java/biblioteca/models/Gestor.java
+++ src/main/java/biblioteca/models/Gestor.java
@@ -4,17 +4,21 @@ import java.util.HashSet;
 
 public abstract class Gestor<E> {
 
+    private HashSet<E> colleccion;
     
     public Gestor() {
+        this.colleccion = new HashSet<E>();
     }
     
     public void add(E item) {
+        this.colleccion.add(item);
     }
     
     public void remove(E item) {
+        this.colleccion.remove(item);
     }
     
     public HashSet<E> getColleccion() {
-        return null;
+        return this.colleccion;
     }
 }
