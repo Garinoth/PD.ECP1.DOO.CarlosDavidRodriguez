6ce581359db4cf0363959b0e7c7ca031d7e38380
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2014-10-10 19:02:29 +0200

    Añadir vista pública de Gestor

---
+++ src/main/java/biblioteca/models/Gestor.java
@@ -0,0 +1,20 @@
+package biblioteca.models;
+
+import java.util.HashSet;
+
+public abstract class Gestor<E> {
+
+    
+    public Gestor() {
+    }
+    
+    public void add(E item) {
+    }
+    
+    public void remove(E item) {
+    }
+    
+    public HashSet<E> getColleccion() {
+        return null;
+    }
+}
