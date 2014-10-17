0cb66f012338cc0ed2848ef3fedc16e29044c665
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2014-10-17 13:59:22 +0200

    Añadir vista pública de Libro

---
+++ src/main/java/biblioteca/models/Libro.java
@@ -0,0 +1,22 @@
+package biblioteca.models;
+
+public class Libro {
+    
+    public Libro(String titulo, String autor, int isbn) {
+        super();
+
+    }
+
+    public String getTitulo() {
+        return null;
+    }
+
+    public String getAutor() {
+        return null;
+    }
+
+    public int getIsbn() {
+        return 0;
+    }
+
+}
