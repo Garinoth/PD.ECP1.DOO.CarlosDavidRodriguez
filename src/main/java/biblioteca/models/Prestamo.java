02324a691588deca7f874e690147650ab155cfc3
Carlos David Rodríguez Peña <cdavidrp@gmail.com>
2014-10-17 14:02:51 +0200

    Implementar Prestamo

--- src/main/java/biblioteca/models/Prestamo.java
+++ src/main/java/biblioteca/models/Prestamo.java
@@ -1,9 +1,21 @@
 package biblioteca.models;
 
+import java.time.LocalDate;
+
 public class Prestamo {
+    
+    private static final int PLAZO = 15;
+    
+    private Usuario usuario;
+    private Libro libro;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
 
     public Prestamo(Usuario usuario, Libro libro) {
-
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaInicio = LocalDate.now();
+        this.fechaFin = LocalDate.now().plusDays(Prestamo.PLAZO);
     }
     
     
